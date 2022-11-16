2350aa80b55d463970b3fbc545da824fc8da9b67
imane-aj <ajroudi.im@gmail.com>
2022-11-16 16:19:58 +0100

    [ADD] update

--- prototypes/tp standard/Index.js
+++ prototypes/tp standard/Index.js
@@ -1,24 +1,22 @@
 function Index(){
 
-    // const [tasks, setTasks] = React.useState([]);
+    const [tasks, setTasks] = React.useState([]);
 
-    // React.useEffect(() => {
-    //   axios.get("http://127.0.0.1:8000/api/task").then((res) => {
-    //     setTasks(res.data);
-    //   });
-    // }, []);
+    React.useEffect(() => {
+      axios.get("http://127.0.0.1:8000/api/task").then((res) => {
+        setTasks(res.data);
+      });
+    }, []);
 
 
-    // return (
-    //     <div>
-    //     <div>hey {tasks.map(task =>
-    //     <p>{task.name}</p>
+    return (
+        <div>
+        <div>hey {tasks.map(task =>
+        <p>{task.name}</p>
         
-    //     )}
-    //     </div>
-    // </div>
-    //     );
-    return <h1>hello</h1>
+        )}
+        </div>
+    </div>
+        );
 }
-
 // export default Index
