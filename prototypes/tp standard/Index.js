50bdd018c873e19df888f05b612b0de81d2c0120
imane-aj <ajroudi.im@gmail.com>
2022-11-17 11:41:03 +0100

    [ADD] delete

--- prototypes/tp standard/Index.js
+++ prototypes/tp standard/Index.js
@@ -8,15 +8,48 @@ function Index(){
       });
     }, []);
 
+    const onDelete = (id) => {
+      axios.delete(`http://127.0.0.1:8000/api/task/${id}`)
+      .then(() => {
+        window.location.reload(false);
+          console.log('done');
+      })
+    }
+
 
     return (
-        <div>
-        <div>hey {tasks.map(task =>
-        <p>{task.name}</p>
-        
-        )}
-        </div>
+      <div className="container">
+      <div className='row justify-content-center'>
+          <div className='hero'>
+          <form action="" method="POST">
+                <div className="input-group mb-3">
+                  <input type="text" className="form-control" name="task" />
+                  <input type="submit" className="add" value="submit" name="submit" />
+                </div>
+            </form>
+            <table className="table" key={Math.random()}>
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col" className="end">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {tasks.map(task =>   
+                  <tr>
+                    <td>{task.name}</td>
+                    <td><button className="btn btn-danger" onClick={() => onDelete(task.id)}>Delete</button> </td>
+                  </tr>
+                  )}
+                </tbody>
+            </table>
+          </div>
+      </div>
     </div>
+    //     <div>
+    //     <div>hey 
+    //     </div>
+    // </div>
         );
 }
 // export default Index
